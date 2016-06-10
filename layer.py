05f4b05447d0c0ac46fd36497dcf675d57328376
Sgit Kene <sgitkene@gmail.com>
2016-06-10 15:15:48 +0200

    adapted to new yowsup version

--- layer.py
+++ layer.py
@@ -1,23 +1,50 @@
-from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
-from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
-from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
-from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
-from pytg.sender import Sender
+from yowsup.layers.interface                          import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+from yowsup.layers.protocol_acks.protocolentities     import *
+from yowsup.layers.protocol_presence                  import YowPresenceProtocolLayer
+from yowsup.layers.protocol_media                     import YowMediaProtocolLayer
+from yowsup.layers.protocol_receipts                  import YowReceiptProtocolLayer
+from yowsup.layers.protocol_groups                    import YowGroupsProtocolLayer
+from yowsup.layers.protocol_ib                        import YowIbProtocolLayer
+from yowsup.layers.protocol_notifications             import YowNotificationsProtocolLayer
+from yowsup.layers.protocol_iq                        import YowIqProtocolLayer
+from yowsup.layers.protocol_contacts                  import YowContactsIqProtocolLayer
+from yowsup.layers.protocol_chatstate                 import YowChatstateProtocolLayer
+from yowsup.layers.protocol_privacy                   import YowPrivacyProtocolLayer
+from yowsup.layers.protocol_profiles                  import YowProfilesProtocolLayer
 
-class EchoLayer(YowInterfaceLayer):
+from pytg.receiver import Receiver
+from pytg.utils import coroutine
+
+import threading
+
+class RelayLayer(YowInterfaceLayer):
     def __init__(self):
-        super(EchoLayer, self).__init__()
-        self.sender = Sender(host="localhost", port=4458)
+        super(RelayLayer, self).__init__()
+        self.receiver = Receiver (host="localhost", port=4458)
+        self.QUIT = False
+        self.receiver.start()
+        self.rcv = threading.Thread(target = self.startReceiver)
+        self.rcv.daemon = True
 
-    @ProtocolEntityCallback("message")
-    def onMessage(self, messageProtocolEntity):
-        #send receipt otherwise we keep receiving the same message over and over
-        if True:
-            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
-            self.toLower(receipt)
-        self.sender.msg(messageProtocolEntity.getFrom(False), messageProtocolEntity.getBody())
+    def startReceiver(self):
+        self.receiver.message(self.main_loop())
 
-    @ProtocolEntityCallback("receipt")
-    def onReceipt(self, entity):
-        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
-        self.toLower(ack)
+    @ProtocolEntityCallback("success")  # we get this at the start of yowsup, only once.
+    def onSuccess(self, ProtocolEntity):
+        self.rcv.start()
+
+    @coroutine
+    def main_loop(self):
+        while True:
+            msg = (yield)
+            if msg.event != "message":     #not a message
+                continue         
+            if msg.own:                    #message from us
+                continue
+            if msg.text == None:           #message not text
+                continue
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(    #currently broken, will crash on emoji and other symbols.
+                msg.text, to = msg.peer.name + "@s.whatsapp.net")
+            self.toLower(outgoingMessageProtocolEntity)
+        
--- run.py
+++ run.py
@@ -1,37 +1,22 @@
 from layer2 import RelayLayer
-from layer import EchoLayer
-from yowsup.layers.auth                        import YowCryptLayer, YowAuthenticationProtocolLayer
-from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
-from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
-from yowsup.layers.protocol_acks               import YowAckProtocolLayer
-from yowsup.layers.network                     import YowNetworkLayer
-from yowsup.layers.coder                       import YowCoderLayer
-from yowsup.layers.stanzaregulator             import YowStanzaRegulator
+from credentials import CREDENTIALS
 
-
-
-from yowsup.stacks import YowStack
-from yowsup.common import YowConstants
+from yowsup.stacks import YowStackBuilder
+from yowsup.layers.auth import AuthError
 from yowsup.layers import YowLayerEvent
-from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS, YOWSUP_PROTOCOL_LAYERS_BASIC
-from yowsup import env
+from yowsup.layers.network import YowNetworkLayer
+from yowsup.env import YowsupEnv
 
-
-
-CREDENTIALS = ("PHONE", "PASSWORD") # replace with your phone and password
 
 if __name__==  "__main__":
-    layers = (
-        (RelayLayer,
-        EchoLayer),
-        YOWSUP_PROTOCOL_LAYERS_BASIC
-        ) + (YowCoderLayer, YowCryptLayer, YowStanzaRegulator, YowNetworkLayer)
-    
-    stack = YowStack(layers)
-    stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, CREDENTIALS)         #setting credentials
-    stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])    #whatsapp server address
-    stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)              
-    stack.setProp(YowCoderLayer.PROP_RESOURCE, env.CURRENT_ENV.getResource())          #info about us as WhatsApp client
+    stackBuilder = YowStackBuilder()
+
+    stack = stackBuilder\
+        .pushDefaultLayers(True)\
+        .push(RelayLayer)\
+        .build()
+
+    stack.setCredentials(CREDENTIALS) 
 
     stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))   #sending the connect signal
 
